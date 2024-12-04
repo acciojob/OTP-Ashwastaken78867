4d4a6dce6dcbebbf442336cf2ae74d95fbe8a1e3
Bagda@Sh <168397146+Ashwastaken78867@users.noreply.github.com>
2024-12-04 11:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+// Get all the input fields with the class 'code'
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+  // Add event listener for when a user types a number
+  input.addEventListener('input', function (e) {
+    if (e.target.value) {
+      // Move to the next input when a value is entered
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    }
+  });
+
+  // Add event listener for backspace to move focus back
+  input.addEventListener('keydown', function (e) {
+    if (e.key === 'Backspace') {
+      if (index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
+});
+
+// Focus on the first input initially
+inputs[0].focus();
+
